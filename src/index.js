218a65427f2f88361b9484bdc4ddd42a8b741e3b
Jefte Keller <kellergnr@gmail.com>
2021-06-22 00:22:31 -0300

    feat: finished CRUD Routes for User Creation and Tasks Manager.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,115 @@
-const express = require('express');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-// const users = [];
+const users = []
 
-function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+function checksExistsUserAccount (request, response, next) {
+    const { username: requestedUsername } = request.headers
+
+    const requestedUser = users.find(user => user.username === requestedUsername)
+
+    if (!requestedUser) {
+        return response.status(404).json({
+            error: 'User not found!'
+        })
+    }
+    request.user = requestedUser
+
+    return next()
+}
+
+function checksExistsTask (request, response, next) {
+    const userTasks = request.user.todos
+    const { id: taskId } = request.params
+
+    const requestedTask = userTasks.find(task => task.id === taskId)
+
+    if (!requestedTask) {
+        return response.status(404).json({
+            error: 'Task not found!'
+        })
+    }
+    request.requestedTask = requestedTask
+
+    return next()
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
-});
+    const { name, username } = request.body
+
+    const userAlreadyExists = users.some(user => user.username === username)
+
+    if (userAlreadyExists) {
+        return response.status(400).json({
+            error: 'User already exists!'
+        })
+    }
+    const newUser = {
+        id: uuidv4(),
+        name,
+        username,
+        todos: []
+    }
+    users.push(newUser)
+
+    return response.status(201).json(newUser)
+})
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+    const userTasks = request.user.todos
+
+    return response.status(200).json(userTasks)
+})
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+    const userTasks = request.user.todos
+    const { title, deadline } = request.body
 
-app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+    const newTask = {
+        id: uuidv4(),
+        title,
+        done: false,
+        deadline: new Date(deadline),
+        created_at: new Date()
+    }
+    userTasks.push(newTask)
 
-app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+    return response.status(201).json(newTask)
+})
 
-app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
-});
+app.put('/todos/:id', checksExistsUserAccount, checksExistsTask, (request, response) => {
+    const { title, deadline } = request.body
+    const { requestedTask } = request
 
-module.exports = app;
+    requestedTask.title = title
+    requestedTask.deadline = new Date(deadline)
+
+    return response.status(200).json(requestedTask)
+})
+
+app.patch('/todos/:id/done', checksExistsUserAccount, checksExistsTask, (request, response) => {
+    const { requestedTask } = request
+
+    requestedTask.done = true
+
+    return response.status(200).json(requestedTask)
+})
+
+app.delete('/todos/:id', checksExistsUserAccount, checksExistsTask, (request, response) => {
+    const userTasks = request.user.todos
+    const { requestedTask } = request
+
+    const taskIndex = userTasks.indexOf(requestedTask)
+    userTasks.splice(taskIndex, 1)
+
+    return response.status(204).send()
+})
+
+module.exports = app
